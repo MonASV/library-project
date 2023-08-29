21ca5d06fcb56774edf415cee840bafdcd78cea3
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-08-29 12:52:44 +0200

    fix list of books and book details

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const Book = require('../models/book.model');
+const Author = require('../models/Author.model')
 const router = express.Router();
 
 
-router.get("/contact-me", () => {})
+router.get("/contact-me", () => { })
 
 
 /* GET home page */
 router.get("/books", (req, res, next) => {
 
     Book.find()
+        .populate("author")
         .then((booksFromDB) => {
             console.log(booksFromDB)
 
-            res.render("books/books-list", {books: booksFromDB})
+            res.render("books/books-list", { books: booksFromDB })
         })
-        .catch( e => {
-            
+        .catch(e => {
+
             console.log("error getting the list of books from the DB");
             next(e)
         })
 
-  //res.render("index");
+    //res.render("index");
 });
 
 // display from
@@ -44,67 +46,69 @@ router.post("/books/create", (req, res, next) => {
 
     }
     Book.create(newBook)
-    .then(() => {
-        console.log(newBook)
-        res.redirect("/books")
-    })
-    .catch(e => {
-        console.log("error creating new book", e)
-        next(e)
-    })
+            .then(() => {
+            console.log(newBook)
+            res.redirect("/books")
+        })
+        .catch(e => {
+            console.log("error creating new book", e)
+            next(e)
+        })
 })
 
 
 router.get('/books/:bookId/edit', (req, res, next) => {
     const { bookId } = req.params;
-   
+
     Book.findById(bookId)
-      .then(bookToEdit => {
-       res.render('books/book-edit', { book: bookToEdit})
-      })
-      .catch(error => next(error));
-  });
+        .populate("author")
+        .then(bookToEdit => {
+            res.render('books/book-edit', { book: bookToEdit })
+        })
+        .catch(error => next(error));
+});
 
 
 
-  router.post('/books/:bookId/edit', (req, res, next) => {
+router.post('/books/:bookId/edit', (req, res, next) => {
     const { bookId } = req.params;
     const { title, description, author, rating } = req.body;
-   
+
     Book.findByIdAndUpdate(bookId, { title, description, author, rating }, { new: true })
-      .then(updatedBook => res.redirect(`/books/${updatedBook.id}`))
-      .catch(error => next(error));
-  });
+        .then(updatedBook => res.redirect(`/books/${updatedBook.id}`))
+        .catch(error => next(error));
+});
 
 
 
-  router.post('/books/:bookId/delete', (req, res, next) => {
+router.post('/books/:bookId/delete', (req, res, next) => {
     const { bookId } = req.params;
-   
+
     Book.findByIdAndDelete(bookId)
-      .then(() => res.redirect('/books'))
-      .catch(error => next(error));
-  });
+        .then(() => res.redirect('/books'))
+        .catch(error => next(error));
+});
 
 
 router.get("/books/:bookId", (req, res, next) => {
-    
+
     const id = req.params.bookId
-    
+
     Book.findById(id)
-    .then(bookFromDB => {
+        .populate("author")
+        .then(bookFromDB => {
 
-        res.render("books/book-details", bookFromDB), 
-        console.log(bookFromDB)
-        //res.send(`Hola ${id}`)
+            res.render("books/book-details", bookFromDB),
+                console.log(bookFromDB)
+            //res.send(`Hola ${id}`)
 
-    })
-    .catch( e => {
-            
-        console.log("error getting the list of books from the DB");
-        next(e)
-    })
-    
+        })
+        .catch(e => {
+
+            console.log("error getting the list of books from the DB");
+            next(e)
+        })
+
 })
 
 
