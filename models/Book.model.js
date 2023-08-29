bde43123c80ab64a6ffd6de4e16c771c64d98f51
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-08-29 12:11:27 +0200

    add Author model & update Book model

--- models/Book.model.js
+++ models/Book.model.js
@@ -1,11 +1,16 @@
 const { Schema, model } = require('mongoose');
 
-const bookSchema = new Schema (
+
+const bookSchema = new Schema(
     {
         title: String,
         description: String,
-        author: String,
-        rating: Number
+        rating: Number,
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "Author"
+        }
+        // authors: [authorSchema], //Array of embedded documents || author: authorSchema, //Single embedded document
     },
     {
         timestamps: true
